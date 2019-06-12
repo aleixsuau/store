1ff7f8c7c4c48992f2ce973a04f59054eb36c077
Aleix Suau <info@macrofonoestudio.es>
2019-06-12 09:29:47 +0200

    Moved background image from base.comp to login

--- src/app/core/components/base/base.component.ts
+++ src/app/core/components/base/base.component.ts
@@ -1,4 +1,3 @@
-import { ConfigService } from './../../config/service/config.service';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/core/auth/auth-service/auth.service';
 import { Component, OnInit } from '@angular/core';
@@ -10,15 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BaseComponent implements OnInit {
   user$: Observable<IUser>;
-  appConfig$: Observable<IAppConfig>;
 
   constructor(
     private authService: AuthService,
-    private configService: ConfigService,
   ) { }
 
   ngOnInit() {
     this.user$ = this.authService.user$;
-    this.appConfig$ = this.configService.config$;
   }
 }
--- src/app/features/login/containers/login/login.component.ts
+++ src/app/features/login/containers/login/login.component.ts
@@ -1,10 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { fadeAnimation } from '../../../../shared/animations/animations';
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { AuthService } from 'src/app/core/auth/auth-service/auth.service';
+import { ConfigService } from 'src/app/core/config/service/config.service';
 
 @Component({
   selector: 'app-login',
@@ -27,6 +28,9 @@ export class LoginComponent implements OnInit {
   invalidErrorMessage: boolean;
   keepMeLoggedIn: boolean;
 
+  appConfig$: Observable<IAppConfig>;
+
+
   @HostBinding('@fade') fade = true;
 
   constructor(
@@ -34,9 +38,12 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private formBuilder: FormBuilder,
     private activatedRoute: ActivatedRoute,
+    private configService: ConfigService,
   ) { }
 
   ngOnInit() {
+    this.appConfig$ = this.configService.config$;
+
     this.loginForm = this.formBuilder.group({
       username: [ '', [ Validators.required ]],
       password: [ '', [ Validators.required ]],
