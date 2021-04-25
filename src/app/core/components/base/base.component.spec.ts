ad275d2267ff9d9de8083ea53dd3ebd3e57eec9b
Aleix Suau <info@macrofonoestudio.es>
2021-04-25 11:37:25 +0200

    BaseComponent tests

--- src/app/core/components/base/base.component.spec.ts
+++ src/app/core/components/base/base.component.spec.ts
@@ -1,18 +1,35 @@
+import { RouterModule } from '@angular/router';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
+import { UserService } from '../../services/user/user.service';
 import { BaseComponent } from './base.component';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('BaseComponent', () => {
+
+@Component({
+  selector: 'app-header',
+  template: '',
+})
+class HeaderComponent {
+}
+
+fdescribe('BaseComponent', () => {
   let component: BaseComponent;
   let fixture: ComponentFixture<BaseComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ BaseComponent ]
+      declarations: [
+        BaseComponent,
+        HeaderComponent,
+      ],
+      imports: [
+        RouterModule.forRoot([]),
+      ]
     })
     .compileComponents();
   }));
-
+//
   beforeEach(() => {
     fixture = TestBed.createComponent(BaseComponent);
     component = fixture.componentInstance;
@@ -21,5 +38,23 @@ describe('BaseComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain the app header', () => {
+    const appHeader = fixture.debugElement.query(By.css('app-header'));
+
+    expect(appHeader).toBeTruthy();
+  });
+
+  it('should contain the nav bar', () => {
+    const appNavBar = fixture.debugElement.query(By.css('nav[mat-tab-nav-bar]'));
+
+    expect(appNavBar).toBeTruthy();
+  });
+
+  it('should contain one link per', () => {
+    const appNavBarLinks = fixture.debugElement.queryAll(By.css('a[mat-tab-link]'));
+
+    expect(appNavBarLinks.length).toBe(component.navLinks.length);
   });
 });
